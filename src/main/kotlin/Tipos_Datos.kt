6cfef2d87796c56443eccf00b90ee2648fbbbb00
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2023-04-01 11:05:12 -0500

    adding class Tipos_Datos.kt

---
+++ src/main/kotlin/Tipos_Datos.kt
@@ -0,0 +1,12 @@
+fun main(){
+    /**
+     * Numericos -> int, long, short, byte
+     * Decimales -> double, float
+     * Cadenas -> string
+     * Char -> char
+     * Booleanos -> bool - true o false
+     */
+
+    val valor: Int
+    
+}
