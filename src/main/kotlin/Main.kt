c9e09c16e84747ef5f73945b684b90203d324eab
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2023-04-01 10:53:34 -0500

    adding lines of code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>){
-    println("Hello world!")
+    println("Hola Programador! Como estas?")
+    println("Estoy bien! y tu?")
+    println("Bastante bien! Estoy aprendiendo a utilizar el lenguaje KOTLIN!")
 }
